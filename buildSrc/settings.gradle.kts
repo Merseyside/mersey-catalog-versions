478d5c60406d2b035e81f1f1dbcb273679c9a7dc
Merseyside <ivanklessablin@gmail.com>
2021-09-19 12:40:26 +0700

    ++ver 1.0.8 Add full versions catalog

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -4,5 +4,6 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 dependencyResolutionManagement {
     versionCatalogs {
         create("gradlePluginsLibs") { from(files("gradle-plugins.libs.versions.toml")) }
+        create("libs") { from(files("../catalog-version-full/full.versions.toml")) }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,6 @@ include(":catalog-version-common")
 include(":catalog-version-android")
 include(":catalog-version-multiplatform")
 
+include(":catalog-version-full")
+
 rootProject.name = "mersey-catalog-versions"
