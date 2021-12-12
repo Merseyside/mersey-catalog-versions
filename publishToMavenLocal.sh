4c309796eac6776c51e5bfc8fc3d105bc620c044
Merseyside <ivanklessablin@gmail.com>
2021-12-12 16:23:17 +0700

    ++ver 1.2.1 Up mersey libs

--- publishToMavenLocal.sh
+++ publishToMavenLocal.sh
@@ -1,2 +1,4 @@
 ./gradlew -PlibraryPublish :catalog-version-android:publishToMavenLocal
 ./gradlew -PlibraryPublish :catalog-version-common:publishToMavenLocal
+./gradlew -PlibraryPublish :catalog-version-multiplatform:publishToMavenLocal
+./gradlew -PlibraryPublish :catalog-version-full:publishToMavenLocal
