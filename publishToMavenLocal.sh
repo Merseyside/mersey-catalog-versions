c9f57f5de4d45ca039bb86a1bb4d54e4ea15fe71
Ivan Sablin <ivansablin@MacBook-Pro-Ivan.local>
2022-02-25 13:59:24 +0700

    Add gradle/plugins catalogs

--- publishToMavenLocal.sh
+++ publishToMavenLocal.sh
@@ -1,4 +1,6 @@
 ./gradlew -PlibraryPublish :catalog-version-android:publishToMavenLocal
 ./gradlew -PlibraryPublish :catalog-version-common:publishToMavenLocal
 ./gradlew -PlibraryPublish :catalog-version-multiplatform:publishToMavenLocal
+./gradlew -PlibraryPublish :catalog-version-gradle:publishToMavenLocal
+./gradlew -PlibraryPublish :catalog-version-plugins:publishToMavenLocal
 ./gradlew -PlibraryPublish :catalog-version-full:publishToMavenLocal
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,8 @@
 include(":catalog-version-common")
 include(":catalog-version-android")
 include(":catalog-version-multiplatform")
+include(":catalog-version-gradle")
+include(":catalog-version-plugins")
 
 include(":catalog-version-full")
 
