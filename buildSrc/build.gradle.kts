3762f060b7179b6d11116f28e3eca689099f4b36
Ivan Sablin <ivansablin@MacBook-Pro-Ivan.local>
2022-04-04 18:33:56 +0700

    ++ver 1.4.1 Up kotlin, mersey kmm lib, kswift

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,6 +12,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20-RC2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
 }
