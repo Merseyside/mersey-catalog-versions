311dc11e3be88fd205d5429cc35847d841d2aa41
Ivan Sablin <ivansablin@MacBook-Pro-Ivan.local>
2022-12-29 16:41:39 +0600

    ++ver 1.6.4 Up versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
 }
