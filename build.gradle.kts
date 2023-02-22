452f51d74d884a8d901e91e31a0aab4935dd9f42
Ivan Sablin <ivansablin@MacBook-Pro-Ivan.local>
2023-02-22 18:03:45 +0600

    ++ver 1.6.7 Up versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 
 allprojects {
     group = "io.github.merseyside"
-    version = "1.6.6"
+    version = "1.6.7"
 }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
 }
