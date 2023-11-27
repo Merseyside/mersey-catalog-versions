cc14cb93162f8f1a324ece403c932c1215f62093
Merseyside <ivanklessablin@gmail.com>
2023-11-27 12:38:06 +0600

    Up kotlin ++1.9.21, up other libs

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
     implementation("io.github.gradle-nexus:publish-plugin:1.3.0")
 }
