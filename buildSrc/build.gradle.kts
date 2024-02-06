cf7086bf2821862bacca3612e1dcd9cdbac78b31
Merseyside <ivanklessablin@gmail.com>
2024-02-06 13:17:56 +0600

    Add new libs, update versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     implementation("io.github.gradle-nexus:publish-plugin:1.3.0")
 }
